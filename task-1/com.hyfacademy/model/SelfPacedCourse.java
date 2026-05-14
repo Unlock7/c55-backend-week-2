027b171e9075778458062fdeee0ac376edab33be
Dagim02 <dagimvanhaileselassie@gmail.com>
2026-05-14 11:32:29 +0200

    Implement SelfPacedCourse extending Course with estimated hours

--- task-1/com.hyfacademy/model/SelfPacedCourse.java
+++ task-1/com.hyfacademy/model/SelfPacedCourse.java
@@ -1,4 +1,24 @@
 package com.hyfacademy.model;
 
-public class SelfPacedCourse {
+public class SelfPacedCourse extends Course {
+    private int estimatedHours;
+
+    public  SelfPacedCourse (String courseName, int maxStudents, int estimatedHours) {
+        super(courseName, maxStudents);
+        this.estimatedHours = estimatedHours;
+    }
+
+    @Override
+    public String getCourseType() {
+        return "Self-Paced";
+    }
+
+    @Override
+    public String getScheduleInfo() {
+        return "Estimated: " + estimatedHours + " hours — complete at your own pace";
+    }
+
+    public int getEstimatedHours(){
+        return estimatedHours;
+    }
 }
